92c8212c828f5c06d4249c0a9b80f6cbaef991c9
Craig Wann <craig@codezone.io>
2024-03-08 10:25:28 -0600

    Adding front-end translations for testing.

--- resources/views/hello.php
+++ resources/views/hello.php
@@ -2,7 +2,7 @@
 
 <div>
     <b>
-		<?php echo esc_attr_x( 'Bible', 'reader', 'bible-plugin' ); ?>
+		<?php echo esc_attr_x( 'Bible', 'scripture', 'bible-plugin' ); ?>
 		<?php echo esc_attr_x( 'Next Chapter', 'reader', 'bible-plugin' ); ?>
 		<?php echo esc_attr_x( 'Previous Chapter', 'reader', 'bible-plugin' ); ?>
     </b>
