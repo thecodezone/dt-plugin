209286fe4942a70b217e540a7cc9c96dd08d901c
corsacca <corsacca@gmail.com>
2024-03-26 10:43:10 +0100

    Fix plugin URL

--- dt-plugin.php
+++ dt-plugin.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: DT Plugin
- * Plugin URI: https://github.com/TheCodeZone/dt_plugin
+ * Plugin URI: https://github.com/thecodezone/dt_plugin
  * Description: A modern disciple.tools plugin starter template.
  * Text Domain: dt-plugin
  * Domain Path: /languages
  * Version:  0.1
  * Author URI: https://github.com/TheCodeZone
- * GitHub Plugin URI: https://github.com/TheCodeZone/dt_plugin
+ * GitHub Plugin URI: https://github.com/thecodezone/dt_plugin
  * Requires at least: 4.7.0
  * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
  * Tested up to: 5.6
