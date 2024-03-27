6a351d9dd35565aaaa46e39540766f6c0fa7384e
Craig Wann <craig@codezone.io>
2024-03-27 10:22:22 -0500

    Handling bug where the rename script changed a function name.

--- .rename.sh
+++ .rename.sh
@@ -1,14 +1,16 @@
 NAME="DT Plugin"
 NAMESPACE="DT\\Plugin"
-PACKAGE="dt\/plugin"
 NAMESPACE_ESCAPED="DT\\\\Plugin"
+PACKAGE="dt\/plugin"
 FILENAME="dt-plugin.php"
 SNAKE_CASE="dt_plugin"
 UPPER_CAMEL_CASE="DT_Plugin"
 KEBAB_CASE="dt-plugin"
 
 # Exclude specified directories
-EXCLUDE_DIRS="-path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./.idea"
+EXCLUDE_DIRS="-path ./vendor -o -path ./vendor-scoped -o -path ./node_modules -o -path ./.git -o -path ./.idea"
+
+PLACEHOLDER="tmp-plugin-name-matcher"
 
 # Replace strings in files excluding specified directories
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT Plugin/$NAME/g"
@@ -16,7 +18,9 @@ find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\Plugin/$NAMESPACE/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\\\Plugin/$NAMESPACE_ESCAPED/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT_Plugin/$UPPER_CAMEL_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_plugins/$PLACEHOLDER/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_plugin/$SNAKE_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/$PLACEHOLDER/dt_plugins/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt-plugin/$KEBAB_CASE/g"
 
 mv dt-plugin.php $FILENAME
