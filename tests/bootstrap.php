ecfefd9c84c8245a419887ab5a852df400f8ecb3
Craig Wann <craig@codezone.io>
2024-06-05 20:49:14 -0500

    Fixing plugin bootstrap path.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 // phpcs:disable
 $_tests_dir   = $_ENV['WP_TESTS_DIR'] ?? false ?: rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 $_core_dir    = $_ENV['WP_CORE_DIR'] ?? false ?: rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress';
-$_plugin_file = $_ENV['WP_PLUGIN_FILE'] ?? false ?: dirname(__DIR__) . '/' . substr( getcwd(), strrpos( getcwd(), '/' ) + 1 ) . '.php';
+$_plugin_file = $_ENV['WP_PLUGIN_FILE'] ?? false ?: dirname(__DIR__) . '/bible-plugin.php';
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find " . $_tests_dir . "/includes/functions.php, have you run tests/install-wp-tests.sh ?" . PHP_EOL; //@phpcs:ignore
@@ -27,4 +27,3 @@ tests_add_filter( 'muplugins_loaded', $_register_plugin );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
