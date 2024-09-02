7f8628d32436b4250ec4b1eab1a6901d73cbd97b
Craig Wann <craig@codezone.io>
2024-09-02 09:48:23 -0500

    Switching route files to use route collection interface.

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,19 @@
  *
  * Routes are used to bind a URL to a controller.
  *
- * @var Router $r
+ * @var RouteCollectionInterface $r
  * @see https://github.com/thecodezone/wp-router
  */
 
+use DT\Plugin\CodeZone\WPSupport\Middleware\HasCap;
 use DT\Plugin\Controllers\HelloController;
 use DT\Plugin\Controllers\UserController;
-use DT\Plugin\League\Route\RouteGroup;
-use DT\Plugin\League\Route\Router;
+use DT\Plugin\League\Route\RouteCollectionInterface;
 use DT\Plugin\Middleware\LoggedIn;
-use DT\Plugin\CodeZone\WPSupport\Middleware\HasCap;
 
-$r->group( '', function ( RouteGroup $r ) {
-    $r->get( '/hello', [ HelloController::class, 'show' ] );
-    $r->get( '/users/me', [ UserController::class, 'current' ] );
-    $r->get( '/users/{id}', [ UserController::class, 'show' ] )
-        ->middleware( new HasCap( 'dt_list_users' ) );
+$r->group( '', function ( RouteCollectionInterface $r ) {
+	$r->get( '/hello', [ HelloController::class, 'show' ] );
+	$r->get( '/users/me', [ UserController::class, 'current' ] );
+	$r->get( '/users/{id}', [ UserController::class, 'show' ] )
+	  ->middleware( new HasCap( 'dt_list_users' ) );
 } )->middleware( new LoggedIn() );
