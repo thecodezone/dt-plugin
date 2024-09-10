f219fbda33aef42a00f229a38afb1c3ef5651411
Craig Wann <craig@codezone.io>
2024-09-10 18:05:20 -0500

    Adding extract request input test

---
+++ tests/ExtractRequestInputTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+class ExtractRequestInputTest extends TestCase {
+
+
+	/**
+	 * @test
+	 */
+	public function it_should_extract_request_input() {
+		$this->assertTrue( true );
+	}
+
+}
